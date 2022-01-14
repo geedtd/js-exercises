2282180861a36882ea4e9627f38cdc697e2913ca
geedtd <grcazares@gmail.com>
2022-01-14 00:40:52 -0800

    Starting of review exercises

---
+++ app.js
@@ -0,0 +1,47 @@
+// console.log('Hello')
+
+// const names = ['anna', 'susy', 'bob']
+// const lastName = 'fitzgerald'
+// let newArray = [];
+
+
+// for (let i = 0; i < names.length; i++) {
+//     console.log(i)
+//     console.log(names[i])
+//     newArray.push(`${names[i]} ${lastName}`)
+// }
+
+// console.log(newArray)
+
+// const gas  = [10, 20, 30 ,40]
+// const food = [11, 12, 13]
+
+// function calculateTotal(arr) {
+//     let total = 0   
+//     for( let i=0 ; i < arr.length; i++) {
+//         // console.log(arr[i])
+//         total += (arr[i])
+//         // console.log(total)
+//     }
+//     return total
+// }
+
+// const gasTotal = calculateTotal(gas)
+// const foodTotal = calculateTotal(food)
+
+// console.log({
+//     gas: gasTotal,
+//     food: foodTotal
+// })
+
+
+// let name = 'bobo';
+// name = 'peter';
+
+// function change() {
+//     let name = 'milton'
+//     console.log(name);
+// }
+
+// change()
+// console.log(name)
