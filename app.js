025ae5362ccdf61ec83df7b98ce292196365e93b
geedtd <grcazares@gmail.com>
2022-01-15 03:07:57 -0800

    did more review exercises

--- app.js
+++ app.js
@@ -45,3 +45,61 @@
 
 // change()
 // console.log(name)
+
+// const people = [
+//     {name: 'gerardo', age: 27 , position: 'SWE'},
+//     {name: 'Drew', age: 23 , position: 'UI Designer'},
+//     {name: 'Mildred', age: 33, position: 'Product Manager'}
+// ]
+
+// function showPerson(item) {
+//     console.log(item.position)
+// }
+
+// people.forEach(showPerson)
+
+// let total = 1112.57 + 547.81 + 15.75
+// console.log(total)
+// let realTotal = total - 149.30
+// console.log(realTotal)
+// let allTotals = realTotal + 4613.81
+// console.log(allTotals)
+
+const people = [
+    {name: 'gerardo', age: 27 , position: 'SWE'},
+    {name: 'Drew', age: 23 , position: 'UI Designer'},
+    {name: 'Mildred', age: 33, position: 'Product Manager'}
+]
+
+const roles = people.filter(function(item) {
+    return item.position === 'SWE'
+})
+
+console.log(roles)
+
+// function showPerson(item) {
+//     console.log(item.name)
+// }
+
+// people.forEach(showPerson)
+
+// const ages = people.map(function(item) {
+//     console.log(item)
+//     return item.age +25
+// })
+
+// const newDetails = people.map(function(item) {
+//     return {
+//         firstName: item.name.toUpperCase(),
+//         littleOlder: item.age + 30,
+//     }
+// })
+
+// const names = people.map(function(item) {
+//     return `<h1>${item.name}</h1>`
+// })
+
+// document.body.innerHTML = names.join('')
+
+// console.log(ages)
+// console.log(newDetails)
